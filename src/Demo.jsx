ef828c17cbf10be9edccde55eb7106eb814d90e7
Josh Howard <JoshHoward.Dev@gmail.com>
2023-02-08 17:43:12 -0500

    Upgrade demo video to HD.
    Co-authored-by: Anthony Cho <cho.anthony12@gmail.com>
    Co-authored-by: Brian Jaber <brianjaber@gmail.com>
    Co-authored-by: Chris Oakes <ckoakes1@gmail.com>

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -4,7 +4,7 @@ function Demo() {
 
   return (
     <div className="demoContainer">
-      <iframe width="1120" height="630" src="https://www.youtube.com/embed/PAahF31e92I"
+      <iframe width="1120" height="630" src="https://www.youtube.com/embed/G-eN4C770FM"
         title="YouTube video player" frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
       </iframe>
